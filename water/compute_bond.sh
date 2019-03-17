24034f9b0cdb5265b8f4a2662be901ba02924b5d
Charlles Abreu <abreu@eq.ufrj.br>
2019-03-17 19:51:38 -0400

    Impulse-Langevin results

--- water/compute_bond.sh
+++ water/compute_bond.sh
@@ -7,7 +7,7 @@ cd $dir/$name
 mkdir -p bdf
 cd bdf
 root=../../../..
-#$root/travis/exe/travis -i $root/water/travis_bond.inp -p ../$name.xyz > ${name}_bond.output
+$root/travis/exe/travis -i $root/water/travis_bond.inp -p ../$name.xyz > ${name}_bond.output
 file="rdf_H2O_#2_[Or_Hr].csv"
 sed 's/;/,/g' $file > $root/water/$dir/results/${name}_bond.csv
 
