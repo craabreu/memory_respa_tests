b7aeebb237da2bd1ee6a3de49306850e125fd693
Charlles Abreu <abreu@eq.ufrj.br>
2019-03-17 01:50:44 -0400

    Setup of memory langevin simulations

---
+++ water/langevin/prince_jobs_2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:4
+#SBATCH --time=10:00:00
+#SBATCH --mem=10GB
+#SBATCH --job-name=langevin-2
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ca2356@nyu.edu
+#SBATCH --output=slurm_%j.out
+
+module purge
+module load cuda/9.2.88
+parallel ./run.sh ::: 4 5
+parallel ./run.sh ::: 6 7
