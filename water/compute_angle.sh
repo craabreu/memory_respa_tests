69a186838d9314ec347b1d014807539a98211fba
Charlles Abreu <abreu@eq.ufrj.br>
2019-03-17 13:54:55 -0400

    Corrected bond and angle results

--- water/compute_angle.sh
+++ water/compute_angle.sh
@@ -7,7 +7,7 @@ cd $dir/$name
 mkdir -p adf
 cd adf
 root=../../../..
-$root/travis/exe/travis -i $root/water/travis_angle.inp -p ../$name.xyz > ${name}_angle.output
+#$root/travis/exe/travis -i $root/water/travis_angle.inp -p ../$name.xyz > ${name}_angle.output
 file="adf_H2O_#2_[O1r_H1r]-[O1r_H2r].csv"
 sed 's/;/,/g' $file > $root/water/$dir/results/${name}_angle.csv
 
