f64ac330d7796a3673032ee633d76cc331f78fab
Charlles Abreu <abreu@eq.ufrj.br>
2019-03-20 17:59:38 -0400

    Radial distribution results

--- ethylene_glycol/compute_angle.sh
+++ ethylene_glycol/compute_angle.sh
@@ -7,11 +7,11 @@ cd $dir/$name
 mkdir -p adf
 cd adf
 root=../../../..
-$root/travis/exe/travis -i $root/water/travis_angle.inp -p ../$name.xyz > ${name}_angle.output
+$root/travis/exe/travis -i $root/ethylene_glycol/travis_angle.inp -p ../$name.xyz > ${name}_angle.output
 file="adf_H2O_#2_[O1r_H1r]-[O1r_H2r].csv"
-sed 's/;/,/g' $file > $root/water/$dir/results/${name}_angle.csv
+sed 's/;/,/g' $file > $root/ethylene_glycol/$dir/results/${name}_angle.csv
 
 values=$(grep "Mean value" ${name}_angle.output | sed -e 's/:/,/g' -e 's/degree/,/g' | cut -d"," -f2,4 | sed 's/ //g')
 dt=$(echo $2 | sed 's/p/./')
 line=$(echo "$dt,$values")
-eval "sed -i 's/^$dt.*/$line/' $root/water/$dir/results/angle_stats.csv"
+eval "sed -i 's/^$dt.*/$line/' $root/ethylene_glycol/$dir/results/angle_stats.csv"
