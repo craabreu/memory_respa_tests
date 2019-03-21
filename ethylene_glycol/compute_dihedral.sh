27fab7597a59004bfefd02dff5e60e475d990b16
Charlles Abreu <abreu@eq.ufrj.br>
2019-03-21 15:54:04 -0400

    Dihedral distributions update

--- ethylene_glycol/compute_dihedral.sh
+++ ethylene_glycol/compute_dihedral.sh
@@ -11,10 +11,12 @@ $root/travis/exe/travis -i $root/ethylene_glycol/travis_dihedral.inp -p ../$name
 
 file="ddf_C2H6O2_#2_[C1r_O1r]-[C2r_O2r]-[C1r_C2r].csv"
 cut -d";" -f1,2 $file > $root/ethylene_glycol/$dir/results/${name}_occo_dihedral.csv
+sed -i 's/;/,/g' $root/ethylene_glycol/$dir/results/${name}_occo_dihedral.csv
 
 file1="ddf_C2H6O2_#2_[C1r_C2r]-[O1r_H1r]-[C1r_O1r].csv"
 file2="ddf_C2H6O2_#2_[C2r_C1r]-[O2r_H2r]-[C2r_O2r].csv"
 paste -d, <(cut -d";" -f1,2 $file1 | sed 's/;/,/g') <(cut -d";" -f2 $file2) > $root/ethylene_glycol/$dir/results/${name}_ccoh_dihedral.csv
+sed -i 's/Occurrence,  Occurrence/Occurrence1,  Occurrence2/g' $root/ethylene_glycol/$dir/results/${name}_ccoh_dihedral.csv
 
 file=${name}_dihedral.output
 file=travis.log
